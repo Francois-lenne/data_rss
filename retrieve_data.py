a884d3dba8fac865d4a79a929968d3a696f480fb
François Lenne <francois.lenne@hotmail.fr>
2025-03-01 12:34:35 +0100

    add the function to retrieve the data

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,17 +1,57 @@
+
+
+ # libraries
 import feedparser
+import pandas as pd
 
-# URL of the RSS feed for the 'r' tag, sorted by newest
-rss_url = "https://stackoverflow.com/feeds/tag?tagnames=r&sort=newest"
 
-# Parse the feed
-feed = feedparser.parse(rss_url)
 
-# Display the feed title
-print(f"Feed Title: {feed.feed.title}\n")
 
-# Loop through the entries and display details
-for entry in feed.entries[:5]:  # Limiting to the first 5 entries
-    print(f"Title: {entry.title}")
-    print(f"Link: {entry.link}")
-    print(f"Published: {entry.published}")
-    print(f"Summary: {entry.summary}\n")
+def get_rss_feed_stackoverflow(tag):
+
+    """
+    Retrieve the data from the stackoverflow feed using feedparser
+
+    :param tag: the subject of the stackoverflow feed
+    :return: the dataframe with the data
+    
+    """
+    # URL of the RSS feed for the 'r' tag, sorted by newest
+    rss_url = f"https://stackoverflow.com/feeds/tag?tagnames={tag}&sort=newest"
+
+    # Parse the feed
+    feed = feedparser.parse(rss_url)
+
+    # Display the feed entries
+    df = pd.DataFrame(columns=[
+        "ID", "Title", "Link", "Published", "Updated", "Summary", "Author", 
+        "Category", "Author Detail", "Tags"
+    ])
+
+    number_of_entries = len(feed.entries)
+    print(f"Number of entries: {number_of_entries}")
+
+    for entry in feed.entries:
+        new_row = {
+            "ID": entry.id,
+            "Title": entry.title,
+            "Link": entry.link,
+            "Published": entry.published,
+            "Updated": entry.updated,
+            "Summary": entry.summary,
+            "Author": entry.author,
+            "Category": getattr(entry, 'category', ''),  # Utilise getattr pour éviter les erreurs si l'attribut n'existe pas
+            "Author Detail": str(entry.author_detail),
+            "Tags": str(entry.tags)
+        }
+
+        # Add the new row to the dataframe
+        df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    
+    # Déplacé ici, en dehors de la boucle
+    return df
+
+
+df = get_rss_feed_stackoverflow("r")
+
+print(f"Dataframe lenght: {len(df)}")
