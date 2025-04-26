f6fc123d61462eb3fa8b8ceca1a52df536df7e37
François Lenne <francois.lenne@hotmail.fr>
2025-04-26 17:49:20 +0200

    finish the janitoring of the r code

--- function/correct_r_code.py
+++ function/correct_r_code.py
@@ -1,100 +1,36 @@
 import re
+import dataclasses
+
 
 class r_code:
 
     def __init__(self, r_code: str):
         self.r_code  = r_code
 
-    @staticmethod
-    def retrieve_line_index_code(self,r_code: str) -> dict:
-        """
+def add_comment_code(texte:str, caractere_a_ajouter:str ) -> str:
+    """
+    Put in comment all the lines thar are not in the <code> and </code></pre> tags
+    :param texte: the code as a string
+    :param caractere_a_ajouter: the character to add at the beginning of the line in order to comment it
+    :return: the code with the comments added
+    """
+    # add the carriage return before the <code> and </code></pre> tags
+    texte = re.sub(r'(?=<code>)', '\n', texte)
+    texte = re.sub(r'(?=</code></pre>)', '\n', texte)
+    texte = re.sub(r'(<code>)', r'\1\n', texte)
+    texte = re.sub(r'(</code></pre>)', r'\1\n', texte)
 
-        retrieve a dict of the emplacement of the balise <code> and </code></pre> in the R file
-        :param r_code: the R code as a string
-        :return: a dict with the index of the <code> and </code></pre> tags
-        """
-        # Initialize an empty dict to store the positions
-        ref_ligne_code = {
-            "begin_code": [],
-            "and_code": []
-        }
-        # add a carriage return before the <code> and </code></pre> tags
-        r_code = re.sub(r'(?=<code>)', '\n', r_code)
+    lignes = texte.splitlines() # separate the lines for the for loop
+    to_comment = True # initialisation de la variable to_comment
 
-        r_code = re.sub(r'(?=</code></pre>)', '\n', r_code)
+    for i, ligne in enumerate(lignes):
+        if ligne.strip() == "<code>":
+            to_comment = False
+        elif ligne.strip() == "</code></pre>":
+            to_comment = True
+        if to_comment and ligne.strip() not in ["<code>", "", "</code></pre>"]:
+            lignes[i] = lignes[i][:0] + caractere_a_ajouter + lignes[i][0:]
 
-        # add a carriage return after the <code> and </code></pre> tags
+    texte_modifie = "\n".join(lignes)
 
-        r_code = re.sub(r'(<code>)', r'\1\n', r_code)
-
-        r_code = re.sub(r'(</code></pre>)', r'\1\n', r_code)
-
-        for i, line in enumerate(r_code.splitlines()):  # start à 1 si tu veux une indexation humaine
-            if line == "<code>":
-                ref_ligne_code["begin_code"].append(i)
-
-            if line == "</code></pre>":
-                ref_ligne_code["and_code"].append(i)
-
-        return ref_ligne_code
-    
-
-    @staticmethod
-    def janitor_code(self,r_code: str) -> str:
-        """
-        Clean the R code by removing the <code> and </code></pre> tags
-        :param r_code: the R code as a string
-        :return: the cleaned R code as a string
-        """
-        # remove the <code> and </code></pre> tags
-        r_code = re.sub(r'<code>', '', r_code)
-        r_code = re.sub(r'</code></pre>', '', r_code)
-        # remove the <pre> and </pre> tags
-        r_code = re.sub(r'<pre>', '', r_code)
-        r_code = re.sub(r'</pre>', '', r_code)
-
-        bornes_code = r_code.retrieve_line_index_code(r_code)
-
-        code_list = []
-
-    # Maintenant, vérifions chaque ligne
-        lignes = r_code.splitlines()
-        
-        for i, line in enumerate(lignes):
-            est_dans_code = False
-        for j in range(len(bornes_code["begin_code"])):
-            debut = bornes_code["begin_code"][j]
-            fin = bornes_code["and_code"][j]
-            if debut <= i <= fin:
-                est_dans_code = True
-                code_list.append(line)
-            
-                break
-    
-        if est_dans_code:
-            print(f"ligne {i} dans le code")
-
-        else:
-            print(f"ligne {i} pas dans le code")
-
-        return r_code
-        
-    
-        
-with open("requirements.txt", "r") as f:
-    lignes = f.readlines()
-
-ligne_a_modifier = [2,4,5]   # attention : index 2 = 3ème ligne
-position = 0
-caractere_a_ajouter = "#"
-
-for numero in ligne_a_modifier:
-    if lignes[numero].strip()  != "":
-        lignes[numero] = (
-            lignes[numero][:position] +
-            caractere_a_ajouter +
-            lignes[numero][position:]
-        )
-
-with open("fichier.txt", "w") as f:
-    f.writelines(lignes)
+    return texte_modifie
